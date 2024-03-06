4e26133cbc99728b7bd062b69097d550e75d9c9b
Renata Castellan <renatacastellan200@gmail.com>
2024-03-06 19:35:22 -0400

    comit

--- ExemploLista/obj/Debug/net8.0/ExemploLista.AssemblyInfo.cs
+++ ExemploLista/obj/Debug/net8.0/ExemploLista.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploLista")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51d3facf651da75fe896a12e97d46ea41ce38a6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploLista")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploLista")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
