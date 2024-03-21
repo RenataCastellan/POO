add8b304e58737f291791af64d190b477dd9d9b3
Renata Castellan <102699024+RenataCastellan@users.noreply.github.com>
2024-03-20 22:22:29 -0400

    Atividade Encapsulamento
    Encapsulamento com Get e Set e uso de lista para armazenar cadastro de produtos

---
+++ AtividadeEncapsulamento/Produto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeEncapsulamento
+{
+    public class Produto
+    {
+        public string codigo;
+        public string nome;
+        private string unidade;
+        private double preco;
+        public int quantidade;
+
+        public void SetPreco(double valor)
+        {
+            if (valor > 0)
+            {
+                this.preco = valor;
+            }
+            else
+            {
+                Console.WriteLine("Insira apenas valores positivos!!");
+            }
+        }
+
+        public double GetPreco() { return preco; }
+
+        public void SetUnidade(int uni)
+        {
+            if (uni == 1)
+            {
+                this.unidade = "LT";
+            }
+            else if (uni == 2)
+            {
+                this.unidade = "CX";
+            }
+            else if(uni == 3)
+            {
+                this.unidade = "KG";
+            }
+            else if(uni == 4)
+            {
+                this.unidade = "UN";
+            }
+            else if(uni == 5)
+            {
+                this.unidade = "MT";
+            }
+            else
+            {
+                Console.WriteLine("Insira um valor válido!");
+            }
+        }
+
+        public string GetUnidade() { return unidade; }
+    }
+}
---
+++ AtividadeEncapsulamento/Program.cs
@@ -0,0 +1,80 @@
+﻿using AtividadeEncapsulamento;
+
+int opcao = 1;
+List<Produto> prod = new List<Produto>();
+Produto produto = new Produto();
+do
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("----------------- CADASTRO DE PRODUTO -----------------");
+    Console.ResetColor();
+    Console.WriteLine();
+
+
+
+    Console.Write("Insira o código: ");
+    produto.codigo = Console.ReadLine();
+    Console.Write("Insira o nome: ");
+    produto.nome = Console.ReadLine();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("---- TIPOS DE UNIDADE ----");
+    Console.WriteLine("1 - LT");
+    Console.WriteLine("2 - CX");
+    Console.WriteLine("3 - KG");
+    Console.WriteLine("4 - UN");
+    Console.WriteLine("5 - MT");
+    Console.WriteLine();
+    Console.ResetColor();
+    Console.Write("Insira a unidade do produto: ");
+    int unidade = int.Parse(Console.ReadLine());
+    produto.SetUnidade(unidade);
+    Console.Write("Insira o preço do produto: ");
+    double preco = double.Parse(Console.ReadLine());
+    produto.SetPreco(preco);
+    Console.Write("Insira a quantidade do produto: ");
+    produto.quantidade = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    prod.Add(produto);
+
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Deseja cadastrar outro produto?");
+    Console.ResetColor();
+    Console.WriteLine("1 - SIM");
+    Console.WriteLine("0 - NÃO");
+    Console.Write("Informe a opção: ");
+    opcao = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+} while (opcao == 1);
+
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("------------- PRODUTOS CADASTRADOS -------------");
+Console.ResetColor();
+
+//lista todos os produtos
+
+foreach (Produto produtos in prod)
+{
+    Console.WriteLine("Codigo: " + produtos.codigo);
+    Console.WriteLine("Nome: " + produtos.nome);
+    Console.WriteLine("Unidade: " + produtos.GetUnidade());
+    Console.WriteLine("Preço: " + produtos.GetPreco());
+    Console.WriteLine("Quantidade: " + produtos.quantidade);
+    Console.WriteLine();
+}
+
+//remove um produto
+
+Console.WriteLine("Digite o id: ");
+string id = Console.ReadLine();
+
+foreach (Produto p in prod)
+{
+    if(produto.codigo == id)
+    {
+        prod.Remove(produto);
+        break;
+    }
+}
